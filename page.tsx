d328ca72378283d4913f084c17506f01701d39d2
Thejan Bandara <hmagthejanbandar@gmail.com>
2026-02-05 21:55:05 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    
+
+     <>
+     <h1 className="text-3xl font-bold">Welcome to the page</h1>
+
+      <ul>
+
+        <li><Link href="/"  className={styles.link}>Home</Link></li>
+        <li><Link href="/services" className={styles.link}>Services</Link></li>
+        <li><Link href="/about" className={styles.link}>About</Link></li>
+        <li><Link href="/contact" className={styles.link}>Contact</Link></li>
+        <li><Link href="/products" className={styles.link}>Products</Link></li>
+        <li><Link href="/about/branches" className={styles.link}>Branches</Link></li>
+        <li><Link href="/products/vegis/" className={styles.link}>vegis</Link></li>
+
+        
+      </ul>
+       <Link href="https://github.com/" target="_blank" id={styles.speicalLink}>GitHub</Link>
+
+
+     </>
+
+
+
+
+      
+  )
+} 
